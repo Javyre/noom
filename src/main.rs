987337370e7c4a719aed363a86660804111890bb
Javier A. Pollak <javi.po.123@gmail.com>
2023-01-26 13:34:59 -0500

    Impl recursive directory build

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    ffi::OsStr,
     fs::File,
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -27,9 +28,10 @@ enum Command {
         /// Input file or directory to build
         input: PathBuf,
 
-        #[arg(short, long = "out", default_value = "./lua/")]
-        /// Output fire or directory
-        output: PathBuf,
+        #[arg(short, long = "out")]
+        /// Output fire or directory. Defaults to ./lua/ and . for directory and file inputs
+        /// respectively.
+        output: Option<PathBuf>,
     },
 }
 
@@ -60,18 +62,40 @@ fn compile_file(input: &Path, output: &Path) {
     }
 }
 
+fn compile_dir(base_path: &Path, input: &Path, output_base: &Path) {
+    for entry in input.read_dir().expect("failed to read directory") {
+        let entry = entry.expect("failed to discover directory entry");
+
+        let path = entry.path();
+        if path.is_dir() {
+            compile_dir(base_path, &path, output_base)
+        } else if path.extension() == Some(OsStr::new("nm")) {
+            let mut output = output_base.to_owned();
+            output.push(path.as_path().strip_prefix(base_path).unwrap());
+            output.set_extension("lua");
+
+            std::fs::create_dir_all(output.as_path().parent().unwrap())
+                .expect("failed to create output dir");
+
+            compile_file(&path, &output);
+        }
+    }
+}
+
 fn main() {
     let args = Cli::parse();
     match args.command {
-        Command::Build { input, mut output } => {
+        Command::Build { input, output } => {
             if input.is_file() {
+                let mut output = output.unwrap_or(".".into());
                 if output.is_dir() {
                     output.push(input.file_name().unwrap());
                     output.set_extension("lua");
                 }
                 compile_file(&input, &output)
             } else {
-                unimplemented!("non-file input")
+                let output = output.unwrap_or("./lua/".into());
+                compile_dir(&input, &input, &output);
             }
         }
     }
